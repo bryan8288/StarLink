20852e8138bc6174481b1822a0078958d6e23666
muhrayyan <muhammadrayyan2001@gmail.com>
2021-11-06 18:17:17 +0700

    add data

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -18,33 +18,33 @@ class CompanySeeder extends Seeder
                 'name' => 'PT. Freeport Indonesia',
                 'address' => 'Jl. Apel No. 11, Jakarta Barat',
                 'phone' => '0813143141',
-                'profile_picture' => 'freeport.jpg'
+                'profile_picture' => 'image/freeport.jpg'
             ),
             array(
                 'user_id' => '4',
                 'name' => 'PT. Grab',
                 'address' => 'Jl. Mawar No. 35, Jakarta Selatan',
                 'phone' => '081287418',
-                'profile_picture' => 'grab.png'
+                'profile_picture' => 'image/grab.png'
             ),array(
                 'user_id' => '8',
                 'name' => 'PT. Gojek Indonesia',
                 'address' => 'Jl. Anggrek No. 9, Jakarta Utara',
                 'phone' => '0816479239',
-                'profile_picture' => 'gojek.png'
+                'profile_picture' => 'image/gojek.png'
             ),array(
                 'user_id' => '11',
                 'name' => 'PT. Indomaret',
                 'address' => 'Jl. Mutiara No. 3, Jakarta Timur',
                 'phone' => '0821641987',
-                'profile_picture' => 'indomaret.png'
+                'profile_picture' => 'image/indomaret.png'
             ),
             array(
                 'user_id' => '3',
                 'name' => 'PT. Alfa Indonesia',
                 'address' => 'Jl. Kemanggisan No. 23, Jakarta Pusat',
                 'phone' => '0829847949',
-                'profile_picture' => 'alfamart.png'
+                'profile_picture' => 'image/alfamart.png'
             )
         ));
     }
